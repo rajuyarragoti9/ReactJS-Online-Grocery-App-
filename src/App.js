5b41080dac453c3310cf1d1fe01571511f6d809b
rajuyarragoti9 <rajuyarragoti9@gmail.com>
2023-12-15 11:25:14 +0530

    productpageday1

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Home from "./PAGES/Home/Home";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import ProductPage from "./PAGES/Product/ProductPage";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/product/:prodid" 
+          element={
+            <ProductPage/>
+          }
+        />
 
         <Route path="*" element={<div>404 Not Found</div>} />
       </Routes>

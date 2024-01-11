ff5a15504cf492042918a8ead2f609fedd508eaf
rajuyarragoti9 <rajuyarragoti9@gmail.com>
2024-01-11 12:52:06 +0530

    'cartday4shippingpayment'

--- src/PAGES/Cart/Cart.js
+++ src/PAGES/Cart/Cart.js
@@ -6,13 +6,18 @@ import Navbar from '../../COMPONENTS/Navbar/Navbar'
 import './Cart.css'
 import './Progress.css'
 import './CartContainer.css'
+import './ShippingContainer.css'
+import './PaymentContainer.css'
+import './OrderSucessfull.css'
 const Cart = () => {
   const [cartdata, setcartdata] = React.useState([])
   const [subtotal, setsubtotal] = React.useState(0)
   const [shipping, setshipping] = React.useState(0)
   const [active, setactive] = React.useState(1)
   const [tax, settax] = React.useState(0)
-
+  const [deliverydate, setdeliverydate] = React.useState(
+    new Date(new Date().getTime() + 2 * 24 * 60 * 60 * 1000).toISOString().split('T')[0]
+  )
 
   const getcartitemsfromlocalstorage = () => {
     let cart = JSON.parse(localStorage.getItem('cart'))
@@ -53,6 +58,21 @@ const Cart = () => {
     localStorage.setItem('cart', JSON.stringify(temp))
     getcartitemsfromlocalstorage()
   }
+
+  const savedaddress = [
+    {
+      AddressLine1: "Address Line 1",
+      AddressLine2: "Address Line 2",
+      AddressLine3: "Address Line 3",
+      postalcode: "123456"
+    },
+    {
+      AddressLine1: "Address Line 1",
+      AddressLine2: "Address Line 2",
+      AddressLine3: "Address Line 3",
+      postalcode: "123456"
+    }
+  ]
   return (
     <div>
       <Navbar reloadnavbar={reloadnavbar} />
@@ -316,6 +336,7 @@ const Cart = () => {
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
                   </svg>
+
                   <p>Your cart is empty</p>
                 </div>
             }
@@ -325,23 +346,104 @@ const Cart = () => {
         {
           active == 2 &&
           <div className='shippingcont'>
-            <p>Shipping cont</p>
+            <div className='selectdate'>
+              <h2 className='mainhead1'>Select Delivery Date</h2>
+              <input
+                min={new Date(new Date().getTime() + 2 * 24 * 60 * 60 * 1000).toISOString().split('T')[0]}
+                type='date'
+                value={deliverydate}
+                onChange={(e) => {
+                  setdeliverydate(e.target.value)
+                }}
+              />
+            </div>
+            <div className='previous'>
+              <h2 className='mainhead1'>Previous Saved Address</h2>
+              {
+                savedaddress.length > 0 ?
+                  savedaddress.map((item, index) => {
+                    return (
+                      <div className='radio' key={index}>
+                        <input type='radio' name='address' id={index} />
+                        <span>
+                          {
+                            item.AddressLine1 + ', ' + item.AddressLine2 + ', ' + item.AddressLine3 + ', ' + item.postalcode
+                          }
+                        </span>
+                      </div>
+                    )
+                  })
+                  :
+                  <div className='emptyaddress'>
+                    <p>No address Found</p>
+                  </div>
+              }
+            </div>
+            <h3>OR</h3>
+            <div className='shippingadd'>
+              <input type='text' placeholder='Address Line 1' />
+              <input type='text' placeholder='Address Line 2' />
+              <input type='text' placeholder='Address Line 3' />
+              <input type='text' placeholder='Postal Code' />
+              <button>Save</button>
+            </div>
+
           </div>
         }
         {
           active == 3 &&
           <div className='paymentcont'>
-            <p>Payment cont</p>
+            <h2 className='mainhead1'>Select Payment Method</h2>
+            <div className='paymenttypes'>
+              <div className='c1'>
+                <input type='radio' name='payment' id='payment1' />
+                <img src='https://www.paypalobjects.com/webstatic/en_US/i/buttons/PP_logo_h_100x26.png'
+                  alt='paypal'
+                />
+              </div>
+              <div className='c1'>
+                <input type='radio' name='payment' id='payment1' />
+                <img src='https://www.paypalobjects.com/webstatic/en_US/i/buttons/PP_logo_h_100x26.png'
+                  alt='paypal'
+                />
+              </div>
+              <div className='c1'>
+                <input type='radio' name='payment' id='payment1' />
+                <img src='https://www.paypalobjects.com/webstatic/en_US/i/buttons/PP_logo_h_100x26.png'
+                  alt='paypal'
+                />
+              </div>
+            </div>
+
+            <div
+              className='paymentagreement'
+            >
+              <input type='checkbox' name='agreement' id='agreement' />
+              <label htmlFor='agreement'>I agree to the terms and conditions</label>
+            </div>
+
+            <div className='c2'>
+              <span>Net Total</span>
+              &nbsp;&nbsp;
+              <span>$ {(subtotal + tax + shipping).toFixed(2)}</span>
+            </div>
           </div>
         }
         {
           active == 4 &&
           <div className='ordersuccessfull'>
-            <p>Order successfull</p>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 01-1.043 3.296 3.745 3.745 0 01-3.296 1.043A3.745 3.745 0 0112 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 01-3.296-1.043 3.745 3.745 0 01-1.043-3.296A3.745 3.745 0 013 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 011.043-3.296 3.746 3.746 0 013.296-1.043A3.746 3.746 0 0112 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 013.296 1.043 3.746 3.746 0 011.043 3.296A3.745 3.745 0 0121 12z" />
+            </svg>
+
+<h2 className='mainhead1'>Order Placed Successfully</h2>
           </div>
         }
 
-       {/* CART BUTTONS */}
+
+
+
+        {/* CART BUTTONS */}
         {
           active == 1 && cartdata.length > 0 &&
           <div className='btns'>
@@ -367,7 +469,8 @@ const Cart = () => {
               }}
             >Next</button>
           </div>
-      }
+        }
+
         {
           active == 3 &&
           <div className='btns'>
@@ -386,9 +489,10 @@ const Cart = () => {
         {
           active == 4 &&
           <div className='btns'>
+            
             <button className='nextbtn'
               onClick={() => {
-                alert('Order placed successfully')
+                window.location.href = '/'
               }}
             >Go To Home</button>
           </div>
